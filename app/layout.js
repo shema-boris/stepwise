24092b6201f44c074f8a45c2ba77cabfb4a8a35f
Shema Boris Benimana <sheboris123@gmail.com>
2025-05-26 10:36:55 -0400

    debugged a few things

--- app/layout.js
+++ app/layout.js
@@ -12,29 +12,31 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} `}
-      >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          > 
-            {/*header*/}
-            <Header/>
-            <main>{children}</main>
-            {/*footer*/}
-            <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with love by RoadsideCoder</p>
-              </div>
-            </footer>          
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      baseTheme:dark
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.className} `}
+        >
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            > 
+              {/*header*/}
+              <Header/>
+              <main className="min-h-screen">{children}</main>
+              {/*footer*/}
+              <footer className="bg-muted/50 py-12">
+                <div className="container mx-auto px-4 text-center text-gray-200">
+                  <p>Made with love by RoadsideCoder</p>
+                </div>
+              </footer>          
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,15 +1,83 @@
 import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+import {Button} from "@/components/ui/button"
 import { SignInButton, SignedIn, SignedOut,UserButton } from '@clerk/nextjs';
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, StarsIcon,PenBox } from 'lucide-react';
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator,DropdownMenuItem } from './ui/dropdown-menu';
 const Header = () => {
   return (
-    <div>
-        <SignedOut>
-            <SignInButton/>
-        </SignedOut>
-        <SignedIn>
-            <UserButton/>
-        </SignedIn>
-    </div>
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+        <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
+            <Link href="/">
+                <Image src="/logo.png" alt="Stepwise logo" width={200} height={60}
+                className="h-12 py-1 w-auto object-contain"/>
+            </Link>
+        
+
+            <div className='flex items-center space-x-2 md:space-x-4'>
+                <SignedIn>
+                    <Link href={"/dashboard"}>
+                        <Button variant="outline">
+                            <LayoutDashboard className='h-4 w-4'/>
+                            <span className='hidden md:block'>Industry Insights</span>
+                            
+                        </Button>
+                    </Link>
+                
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button >
+                            <StarsIcon className='h-4 w-4'/>
+                            <span className='hidden md:block'>Growth Tools</span>
+                            <ChevronDown className='h-4 w-4'/>    
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem>
+                            <Link href={"/resume"} className='flex items-center gap-2'>
+                                <FileText className='h-4 w-4'/>
+                                <span>Build Resume</span>
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Link href={"/ai-cover-letter"} className='flex items-center gap-2'>
+                                <PenBox className='h-4 w-4'/>
+                                Cover Letter
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Link href={"/interview"} className='flex items-center gap-2'>
+                                <GraduationCap className='h-4 w-4'/>
+                                Interview Prep
+                            </Link>
+                        </DropdownMenuItem>    
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                </SignedIn>
+                <SignedOut>
+                    <SignInButton>
+                        <Button variant="outline">Sign In</Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton
+                        appearance={{
+                            elements: {
+                                avatarBox: "w-10 h-10",
+                                userButtonPopoverCard: "shadow-x1",
+                                userPreviewMainIdentifier: "font-semibold",
+                            }
+                        }}
+                        afterSignOutUrl='/'
+                    />
+                </SignedIn>
+        
+            </div>
+        </nav>
+        
+    </header>
   )
 }
 
