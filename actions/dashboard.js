a80d7a9c244ca24b325102d1d5de43ca05029b61
Shema Boris Benimana <sheboris123@gmail.com>
2025-06-07 12:32:53 -0400

    Used gen-ai and added charts and bars to the dasboardView

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -1,9 +1,8 @@
 "use server";
-
+import React from "react";
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
