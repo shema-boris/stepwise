5a8b1319279c2db919cb0b573b662b40faad17c4
Shema Boris Benimana <sheboris123@gmail.com>
2025-06-10 19:41:39 -0400

    Debugged some few lines

---
+++ app/(main)/interview/layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+
+export default function Layout({ children }) {
+  return (
+    <div className="px-5">
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
+      >
+        {children}
+      </Suspense>
+    </div>
+  );
+}
