a30edf0c792a23836501d9ea5da993850f3ae022
Shema Boris Benimana <sheboris123@gmail.com>
2025-10-08 21:21:10 -0500

    Implemented the delete and get coverletter functions

--- actions/cover-letter.js
+++ actions/cover-letter.js
@@ -64,3 +64,58 @@ export async function generateCoverLetter(data) {
     throw new Error("Failed to generate cover letter");
   }
 }
+export async function getCoverLetters() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  return await db.coverLetter.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
+
+export async function getCoverLetter(id) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  return await db.coverLetter.findUnique({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+}
+
+export async function deleteCoverLetter(id) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  return await db.coverLetter.delete({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+}
